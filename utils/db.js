4c7c40d632cd7b5b78ec983346eb2f3113ad2dc1
Ajyol Dhamala <114612254+Ajyol@users.noreply.github.com>
2025-05-04 10:01:45 -0500

    Singleton patten implementation for database connection

---
+++ utils/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+let instance = null;
+
+class Database {
+
+    constructor() {
+        if (!instance) {
+            instance = this;
+            this._connect();
+        }
+        return instance;
+    }
+
+    _connect() {
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => {
+            console.log('Database Connected ^^');
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+
+}
+module.exports = new Database();
